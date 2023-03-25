9b05e6a392c3a67bc141b5c2f5dfe25da9e60c59
Ruslan <rusishss@mail.ru>
2023-03-25 18:59:53 +0600

    3

--- src/controllers/comments-controller.ts
+++ src/controllers/comments-controller.ts
@@ -3,6 +3,7 @@ import {LikeService} from "../domain/like-service";
 import {commentsQueryRepo, CommentsQueryRepo} from "../repositories/comments-query-repository";
 import {Request, Response} from "express";
 import {jwtService} from "../application/jwt-service";
+import {log} from "util";
 
 export class CommentsController {
     constructor(protected commentsService: CommentsService, protected likeService: LikeService, protected commentsQueryRepo: CommentsQueryRepo) {
@@ -54,16 +55,20 @@ export class CommentsController {
     async setLikeStatusForComment(req: Request, res: Response) {
         try {
             const isCommentExist = await commentsQueryRepo.getCommentById(req.params.commentsId.toString())
+            console.log('isCommentExist ', isCommentExist)
             if (!isCommentExist) {
                 res.sendStatus(404)
                 return
             }
             const token = req.headers.authorization!.split(' ')[1]
+            console.log('token ', token)
             const userId = await jwtService.getUserIdFromRefreshToken(token)
+            console.log('userId ', userId)
             let updateCommentLike = await this.likeService.updateCommentLike(
                 userId,
                 req.params.commentsId.toString(),
                 req.body.likeStatus)
+            console.log('updateCommentLike ',updateCommentLike)
             if (updateCommentLike) {
                 res.sendStatus(204)
             } else res.status(400).send('not like')
