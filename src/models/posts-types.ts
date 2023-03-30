43fe56b8bcd318217255610e6eb84488c08af7dd
Ruslan <rusishss@mail.ru>
2023-03-30 17:13:34 +0600

    добавил методы в класс post, включая getOutputType()

--- src/models/posts-types.ts
+++ src/models/posts-types.ts
@@ -11,9 +11,42 @@ export class PostDBType {
                 public likesCount: number,
                 public dislikesCount: number,
                 public myStatus: string,
-                public  likesCollection: Array<likesCollectionType>
-    ) {
+                public  likesCollection: Array<likesCollectionType>) {}
+    countLikesAndDislikes() {
+        return this.likesCollection.reduce((acc, post) => {
+            if (post.status === "Like") {
+                acc.likesCount++;
+            } else if (post.status === "Dislike") {
+                acc.dislikesCount++;
+            }
+            return acc;
+        }, { likesCount: 0, dislikesCount: 0 });
     }
+    getNewestLikes(){
+        return this.likesCollection
+            .filter(n => n.status === 'Like')
+            .sort((a, b) => b.addedAt.localeCompare(a.addedAt))
+            .slice(0, 3)
+    }
+
+    getOutputType() {
+        return {
+            id: this._id.toString(),
+            title: this.title,
+            shortDescription: this.shortDescription,
+            content: this.content,
+            blogId: this.blogId,
+            blogName: this.blogName,
+            createdAt: this.createdAt,
+            extendedLikesInfo: {
+                likesCount: this.countLikesAndDislikes().likesCount,
+                dislikesCount: this.countLikesAndDislikes().dislikesCount,
+                myStatus: this.myStatus,
+                newestLikes: this.getNewestLikes()
+            }
+        }
+    }
+
 }
 
 export type PostTypeOutput = {
--- src/repositories/post-query-repository.ts
+++ src/repositories/post-query-repository.ts
@@ -1,11 +1,10 @@
-
 import {PostDBType, PostTypeOutput} from "../models/posts-types";
 import {PaginationOutputModel} from "../models/models";
-import {sort} from "../application/functions";
-import {skipped} from "../application/functions";
+import {skipped, sort} from "../application/functions";
 import {PostModel} from "../schemes/schemes";
 import {ObjectId} from "mongodb";
 import {injectable} from "inversify";
+import {HydratedDocument} from "mongoose";
 
 
 @injectable()
@@ -24,28 +23,13 @@ export class PostsQueryRepo {
             .sort({[sortBy]: sort(sortDirection)})
             .skip(skipped(pageNumber, pageSize))
             .limit(+pageSize)
-            .lean()
 
         let outPosts = posts.map((post: PostDBType) => {
-            const newestLikes = post.likesCollection
-                .filter(n => n.status === 'Like')
-                .sort((a, b) => b.addedAt.localeCompare(a.addedAt))
-                .slice(0, 3)
-            return {
-                id: post._id.toString(),
-                title: post.title,
-                shortDescription: post.shortDescription,
-                content: post.content,
-                blogId: post.blogId,
-                blogName: post.blogName,
-                createdAt: post.createdAt,
-                extendedLikesInfo: {
-                    likesCount: post.likesCount,
-                    dislikesCount: post.dislikesCount,
-                    myStatus: post.myStatus,
-                    newestLikes: newestLikes
-                }
+            const isUserLikePost = post.likesCollection.find(p => p.userId === userId)
+            if(isUserLikePost){
+                post.myStatus = isUserLikePost.status
             }
+            return post.getOutputType()
         })
 
         let pageCount = Math.ceil(+postsCount / +pageSize)
@@ -58,7 +42,6 @@ export class PostsQueryRepo {
             items: outPosts
         }
         return outputPosts
-
     }
 
     async getAllPostsByBlogsID(
@@ -73,34 +56,16 @@ export class PostsQueryRepo {
             .skip(skipped(pageNumber, pageSize))
             .limit(+pageSize)
             .sort({[sortBy]: sort(sortDirection)})
-            .lean()
 
         let outPosts = posts.map((post: PostDBType) => {
-            const newestLikes = post.likesCollection
-                .filter(n => n.status === 'Like')
-                .sort((a, b) => b.addedAt.localeCompare(a.addedAt))
-                .slice(0, 3)
-            return {
-                id: post._id.toString(),
-                title: post.title,
-                shortDescription: post.shortDescription,
-                content: post.content,
-                blogId: post.blogId,
-                blogName: post.blogName,
-                createdAt: post.createdAt,
-                extendedLikesInfo: {
-                    likesCount: post.likesCount,
-                    dislikesCount: post.dislikesCount,
-                    myStatus: post.myStatus,
-                    newestLikes: newestLikes
-                }
+            const isUserLikePost = post.likesCollection.find(p => p.userId === userId)
+            if(isUserLikePost){
+                post.myStatus = isUserLikePost.status
             }
+            return post.getOutputType()
         })
-
         let postsCount = await PostModel.countDocuments({"blogId": blogId})
-
         let pageCount = Math.ceil(+postsCount / +pageSize)
-
         let outputPosts: PaginationOutputModel<PostTypeOutput>  = {
             pagesCount: pageCount,
             page: +pageNumber,
@@ -116,41 +81,107 @@ export class PostsQueryRepo {
             return null
         }
         let _id = new ObjectId(id)
-        const post: PostDBType | null = await PostModel.findOne({_id: _id})
+        const post: HydratedDocument<PostDBType> | null = await PostModel.findOne({_id: _id})
         if (!post) {
             return null
         }
-        const postLikesCollection = post.likesCollection
-        console.log('userId ', userId)
-        console.log('postLikesCollection ', postLikesCollection)
-
-        const isUserLikePost = postLikesCollection.find(p => p.userId === userId)
-        console.log('isUserLikePost ', isUserLikePost)
-        if(isUserLikePost){
-            post.myStatus = isUserLikePost.status
+        const userPostStatus = post.likesCollection.find(p => p.userId === userId)
+        if(userPostStatus){
+            post.myStatus = userPostStatus.status
         }
-        const newestLikes = post.likesCollection
-            .filter(n => n.status === 'Like')
-            .sort((a, b) => b.addedAt.localeCompare(a.addedAt))
-            .slice(0, 3)
-
-        return {
-            id: post._id.toString(),
-            title: post.title,
-            shortDescription: post.shortDescription,
-            content: post.content,
-            blogId: post.blogId,
-            blogName: post.blogName,
-            createdAt: post.createdAt,
-            extendedLikesInfo: {
-                likesCount: post.likesCount,
-                dislikesCount: post.dislikesCount,
-                myStatus: post.myStatus,
-                newestLikes: newestLikes
-            }
-        }
+        return post.getOutputType()
     }
+
+    // async getAllPostsByBlogsID(
+    //     blogId: string,
+    //     sortBy: string,
+    //     sortDirection: string,
+    //     pageNumber: string,
+    //     pageSize: string,
+    //     userId?: string) {
+    //
+    //     let posts = await PostModel.find({"blogId": blogId})
+    //         .skip(skipped(pageNumber, pageSize))
+    //         .limit(+pageSize)
+    //         .sort({[sortBy]: sort(sortDirection)})
+    //
+    //     let outPosts = posts.map((post: PostDBType) => {
+    //         const isUserLikePost = post.likesCollection.find(p => p.userId === userId)
+    //         if(isUserLikePost){
+    //             post.myStatus = isUserLikePost.status
+    //         }
+    //         const newestLikes = post.getNewestLikes()
+    //         const postsLikesAndDislikes = post.countLikesAndDislikes()
+    //         return {
+    //             id: post._id.toString(),
+    //             title: post.title,
+    //             shortDescription: post.shortDescription,
+    //             content: post.content,
+    //             blogId: post.blogId,
+    //             blogName: post.blogName,
+    //             createdAt: post.createdAt,
+    //             extendedLikesInfo: {
+    //                 likesCount: postsLikesAndDislikes.likesCount,
+    //                 dislikesCount: postsLikesAndDislikes.dislikesCount,
+    //                 myStatus: post.myStatus,
+    //                 newestLikes: newestLikes
+    //             }
+    //         }
+    //     })
+    //
+    //     let postsCount = await PostModel.countDocuments({"blogId": blogId})
+    //
+    //     let pageCount = Math.ceil(+postsCount / +pageSize)
+    //
+    //     let outputPosts: PaginationOutputModel<PostTypeOutput>  = {
+    //         pagesCount: pageCount,
+    //         page: +pageNumber,
+    //         pageSize: +pageSize,
+    //         totalCount: postsCount,
+    //         items: outPosts
+    //     }
+    //     return outputPosts
+    // }
+
+    // async getPostByID(id: string, userId?: string): Promise<PostTypeOutput | null> {
+    //     if (!ObjectId.isValid(id)){
+    //         return null
+    //     }
+    //     let _id = new ObjectId(id)
+    //     const post: PostDBType | null = await PostModel.findOne({_id: _id})
+    //     if (!post) {
+    //         return null
+    //     }
+    //     const postLikesCollection = post.likesCollection
+    //     console.log('userId ', userId)
+    //     console.log('postLikesCollection ', postLikesCollection);
+    //
+    //     const isUserLikePost = postLikesCollection.find(p => p.userId === userId)
+    //     console.log('isUserLikePost ', isUserLikePost)
+    //     if(isUserLikePost){
+    //         post.myStatus = isUserLikePost.status
+    //     }
+    //     console.log(post.myStatus)
+    //     const newestLikes = post.getNewestLikes()
+    //
+    //     const postsLikesAndDislikes = post.countLikesAndDislikes()
+    //
+    //     return {
+    //         id: post._id.toString(),
+    //         title: post.title,
+    //         shortDescription: post.shortDescription,
+    //         content: post.content,
+    //         blogId: post.blogId,
+    //         blogName: post.blogName,
+    //         createdAt: post.createdAt,
+    //         extendedLikesInfo: {
+    //             likesCount: postsLikesAndDislikes.likesCount,
+    //             dislikesCount: postsLikesAndDislikes.dislikesCount,
+    //             myStatus: post.myStatus,
+    //             newestLikes: newestLikes
+    //         }
+    //     }
+    // }
 }
 
-// export const postsQueryRepo = new PostsQueryRepo()х
 
